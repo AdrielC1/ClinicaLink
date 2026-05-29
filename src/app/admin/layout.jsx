7d8deedbf10d30c66ac9e9284fb6cfa22291c142
AhmadTauhida <hida03671@gmail.com>
2026-05-29 23:19:24 +0700

    Refactor : Pengaturan Page Now Can Edit Profile

--- src/app/admin/layout.jsx
+++ src/app/admin/layout.jsx
@@ -1,5 +1,5 @@
 import AppSidebarLayout from "@/components/AppSidebarLayout";
 
 export default function AdminLayout({ children }) {
-  return <AppSidebarLayout role="admin">{children}</AppSidebarLayout>;
+    return <AppSidebarLayout role="admin">{children}</AppSidebarLayout>;
 }
